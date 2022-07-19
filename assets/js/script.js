5aeaf825e7b24137fe0e6233fb3eefa8f83da4bd
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-07-19 11:47:44 -0600

    Got hours to color code based on real time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,42 @@
-//Today's date at top of scheduler
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
+//Variables for
+let currentDay = $("#currentDay");
+let scheduleArea = $(".schedule");
+let timeBlock = $(".time-block");
+
+//Today's date and current hour
+let today = moment().format("dddd, MMMM Do, YYYY");
+let currentHour = moment().format("H");
+
+//Array for saved items
+let toDoItems = [];
+
+//Color code text areas based on time
+function colorTimeBlocks() {
+  timeBlock.each(function () {
+    let thisBlock = $(this);
+    let thisBlockHour = parseInt(thisBlock.attr("data-hour"));
+    if (thisBlockHour == currentHour) {
+      thisBlock.addClass("present").removeClass("past future");
+    }
+    if (thisBlockHour > currentHour) {
+      thisBlock.addClass("future").removeClass("present past");
+    }
+    if (thisBlockHour < currentHour) {
+      thisBlock.addClass("past").removeClass("present future");
+    }
+  });
+}
+
+//Save text area input to local storage
+
+//On Load
+$(document).ready(function () {
+  //display current date
+  currentDay.text(today);
+  colorTimeBlocks();
+
+  //render schedule from local storage
+
+  //when a todo item save button is clicked, save it
+  //   scheduleArea.on("click", "button", saveHandler);
+});
